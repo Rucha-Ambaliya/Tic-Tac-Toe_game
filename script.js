4475ff0fff5d45f3fbc44a2dc9521abf32eb6217
Rucha-Ambaliya <ruchaambaliya@gmail.com>
2023-12-09 16:58:29 +0530

    adaded dark/light mode feature

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+let heading = document.querySelector("h1");
+let modeImg = document.querySelector("#mode-img");
+let mode = "dark";
+let body = document.querySelector("body");
 
 let turnO = true;
 let isWinner = false;
@@ -18,7 +22,34 @@ const winPatterns = [
     [6, 7, 8],
 ];
 
+modeImg.addEventListener("click", () => {
+    if (mode === "dark") {
+        modeImg.src = "images/dark.png";
+        body.style.backgroundColor = "#2a9d8f";
+        boxes.forEach((box) => {
+            box.style.backgroundColor = "#e9c46a";
+            box.style.color = "#000";
+        });
+        resetBtn.style.backgroundColor = "#e76f51";
+        resetBtn.style.color = "#000";
+        heading.style.color = "#550c18";
+        mode = "light";
+    } else {
+        modeImg.src = "images/light.png";
+        body.style.backgroundColor = "#02010a";
+        boxes.forEach((box) => {
+            box.style.backgroundColor = "#006466";
+            box.style.color = "#550c18";
+        });
+        resetBtn.style.backgroundColor = "#848586";
+        resetBtn.style.color = "#0c0f0a";
+        heading.style.color = "#d8572a";
+        mode = "dark";
+    }
+});
+
 const resetGame = () => {
+    turn = 0;
     turnO = true;
     enableBoxes();
     resetBtn.innerText = "Reset";
@@ -78,10 +109,12 @@ const checkWinner = () => {
             if (pos1Val === pos2Val && pos2Val=== pos3Val) {
                 showWinner(pos1Val);
                 isWinner = true;
+                return;
             }
         }
-        if (isWinner !== true && turn === 9) {
+        if (isWinner === false && turn === 9) {
             showDraw();
+            return;
         }
     }
 };
